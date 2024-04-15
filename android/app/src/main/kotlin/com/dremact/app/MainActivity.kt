de5ef8901a46dddc7a6a3500169b91fe5e55d8f9
Alfaz Mecheeri <devalfaz@outlook.com>
2024-04-15 11:21:16 +0530

    init

---
+++ android/app/src/main/kotlin/com/dremact/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dremact.app.dreamact_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:dreamact_mobile/app/app.dart';
+import 'package:dreamact_mobile/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
